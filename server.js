6aa66eb26b65519533cc4cf60e17363acb18069c
Alan Nicholas <alannicholas.an@gmail.com>
2026-03-16 11:07:25 -0300

    fix API

--- server.js
+++ server.js
@@ -9,13 +9,12 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://pomo-flow.vercel.app/"],
+    origin: ["http://localhost:5173", "https://pomo-flow-alan.vercel.app"],
   }),
 );
 
 app.use(express.json());
 app.use("/api/study-time", studyTimeRoutes);
-app.use("/api/settings", settingsRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`API rodando na porta ${PORT}`));
